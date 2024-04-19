9619ef5a0d7071f2702601a87d1b97bb7d909203
PedroMidueno <pedromidueno.dev@gmail.com>
2024-04-19 17:49:07 -0600

    Add first part of auth module

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+
+import { PrismaService } from 'src/common/prisma.service'
+import { JwtPayload } from '../interfaces/JwtPayload.interface'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly prisma: PrismaService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET')
+    })
+  }
+
+  async validate(payload: JwtPayload) {
+    const { id } = payload
+
+    const user = await this.prisma.user.findFirst({ where: { id } })
+
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    return payload
+  }
+}
