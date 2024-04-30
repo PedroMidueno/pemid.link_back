097e397fc1ab8a319e08a3d083a8595a8c0ba3eb
PedroMidueno <pedromidueno.dev@gmail.com>
2024-04-30 17:32:35 -0600

    Add login service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+
+import * as bcrypt from 'bcrypt'
+
+import { LoginDto } from './dto/login.dto'
+import { PrismaService } from 'src/common/prisma.service'
+import { JwtPayload } from './interfaces/JwtPayload.interface'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService
+  ) { }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto
+
+    const user = await this.prisma.users.findFirst({
+      where: { email, deleted: false },
+      select: {
+        id: true,
+        email: true,
+        password: true
+      }
+    })
+
+    if (!user) throw new NotFoundException('User not found')
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Invalid credentials')
+
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id })
+    }
+  }
+
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload)
+
+    return token
+  }
+}
