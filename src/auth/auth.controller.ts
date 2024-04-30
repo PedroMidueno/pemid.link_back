5042523805c6820dfc3bc71fef04f1bba87ff5ad
PedroMidueno <pedromidueno.dev@gmail.com>
2024-04-30 17:31:37 -0600

    Add login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { LoginDto } from './dto/login.dto'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto)
+  }
+
+  // Route for test auth endpoints
+  @Get('test')
+  @UseGuards(AuthGuard())
+  test() {
+    return 'Success'
+  }
 }
