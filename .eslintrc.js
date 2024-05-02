973e29bbe0e1dc382175c1cb103fce92160ef442
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-02 09:47:52 -0600

    Refactor @stylistic/eol-last rule and add @stylistic/dot-location rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     'require-await': 'error',
-    '@stylistic/eol-last': ['warn', 'always'],
+    '@stylistic/eol-last': 'error',
     '@stylistic/no-multiple-empty-lines': 'error',
     '@stylistic/no-trailing-spaces': 'error',
     '@stylistic/quotes': ['error', 'single'],
@@ -37,6 +37,7 @@ module.exports = {
     '@stylistic/array-bracket-newline': ['error', 'consistent'],
     '@stylistic/max-len': ['error', { code: 120 }],
     '@stylistic/key-spacing': 'error',
-    '@stylistic/space-infix-ops': 'error'
+    '@stylistic/space-infix-ops': 'error',
+    '@stylistic/dot-location': ['error', 'property']
   }
 }
