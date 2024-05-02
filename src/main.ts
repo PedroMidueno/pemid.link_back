a515cea3750e5f1666744bdebf2288e05ffc9634
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-02 00:00:34 -0600

    Add config to use ResponseInterceptor globally

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 
 import { AppModule } from './app.module'
+import { ResponseInterceptor } from './interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -14,6 +15,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   )
+
+  app.useGlobalInterceptors(new ResponseInterceptor())
 
   await app.listen(3000)
 }
