f9cb1ec065b4a9da2bf0e7bb1ca76647c3524609
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-06 23:59:20 -0600

    Add implementation test of get user decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+
 import { AuthService } from './auth.service'
 import { LoginDto } from './dto/login.dto'
-import { AuthGuard } from '@nestjs/passport'
+import { GetUser } from 'src/common/decorators/get-user.decorator'
+import { User } from 'src/common/types'
 
 @Controller('auth')
 export class AuthController {
@@ -15,7 +18,7 @@ export class AuthController {
   // Route for test auth endpoints
   @Get('test')
   @UseGuards(AuthGuard())
-  test() {
-    return 'Success'
+  test(@GetUser() user: User) {
+    return { user }
   }
 }
