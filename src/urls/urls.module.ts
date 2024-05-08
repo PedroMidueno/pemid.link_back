287b701d31f7c7d5655265acfe3edddcb2393a80
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-08 00:52:58 -0600

    Add imports of modules that will be used

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
+
+import { CommonModule } from '../common/common.module'
+import { AuthModule } from '../auth/auth.module'
 import { UrlsService } from './urls.service'
 import { UrlsController } from './urls.controller'
 
 @Module({
   controllers: [UrlsController],
-  providers: [UrlsService]
+  providers: [UrlsService],
+  imports: [AuthModule, CommonModule]
 })
 export class UrlsModule {}
