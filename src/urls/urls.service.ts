f7d9d578c5109739ab874cc76c04c64fa8097e4e
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-08 00:54:00 -0600

    Add logic to shorten urls

--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -1,4 +1,37 @@
+import { ShortenUrlDto } from './dto/shorten-url.dto'
+import { PrismaService } from './../common/prisma.service'
 import { Injectable } from '@nestjs/common'
+import { createRandomString } from './helpers/create-random-string.helper'
 
 @Injectable()
-export class UrlsService {}
+export class UrlsService {
+  constructor(private readonly prisma: PrismaService) { }
+
+  async shortenUrl(shortenUrlDto: ShortenUrlDto, userId: number = null) {
+    const { longUrl, customCode } = shortenUrlDto
+
+    const shortCode: string = customCode ?
+      customCode :
+      createRandomString(6)
+
+    const shortUrl = await this.prisma.urls.create({
+      data: {
+        longUrl,
+        shortCode,
+        createdBy: userId ? { connect: { id: userId } } : undefined
+      }
+    })
+
+    return {
+      shortCode: shortUrl.shortCode,
+      originalUrl: shortUrl.longUrl
+    }
+  }
+
+  async deleteUrl(id: number) {
+    // TODO
+    await Promise.resolve()
+
+    return id
+  }
+}
