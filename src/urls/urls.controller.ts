06ec1d6f6bc5cde5d0d0bbd73d20af9aa1e56dd8
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-08 00:52:37 -0600

    Add first endpoints to manage urls shorten

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Delete, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+
 import { UrlsService } from './urls.service'
+import { ShortenUrlDto } from './dto/shorten-url.dto'
+import { GetUser } from 'src/common/decorators/get-user.decorator'
 
 @Controller('urls')
 export class UrlsController {
   constructor(private readonly urlsService: UrlsService) {}
+
+  @Post('shorten-public')
+  shortenUrlPublic(@Body() shortenUrlDto: ShortenUrlDto) {
+    return this.urlsService.shortenUrl(shortenUrlDto)
+  }
+
+  @Post('shorten')
+  @UseGuards(AuthGuard())
+  shortenUrl(@Body() shortenUrlDto: ShortenUrlDto, @GetUser('id') userId: number) {
+    return this.urlsService.shortenUrl(shortenUrlDto, userId)
+  }
+
+  @Delete('delete/:id')
+  @UseGuards(AuthGuard())
+  deleteShortUrl(@Param('id', ParseIntPipe) id: number) {
+    return this.urlsService.deleteUrl(id)
+  }
 }
