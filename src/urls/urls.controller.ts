1dfebd17e199605838b35a870e40442956e44a65
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-09 01:15:02 -0600

    Add custom shorten endpoints

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 import { UrlsService } from './urls.service'
@@ -11,13 +11,25 @@ export class UrlsController {
 
   @Post('shorten-public')
   shortenUrlPublic(@Body() shortenUrlDto: ShortenUrlDto) {
-    return this.urlsService.shortenUrl(shortenUrlDto)
+    return this.urlsService.publicShortenUrl(shortenUrlDto)
   }
 
   @Post('shorten')
   @UseGuards(AuthGuard())
   shortenUrl(@Body() shortenUrlDto: ShortenUrlDto, @GetUser('id') userId: number) {
     return this.urlsService.shortenUrl(shortenUrlDto, userId)
+  }
+
+  @Post('shorten-custom')
+  @UseGuards(AuthGuard())
+  shortenUrlCustom(@Body() shortenUrlDto: ShortenUrlDto, @GetUser('id') userId: number) {
+    return this.urlsService.customShortenUrl(shortenUrlDto, userId)
+  }
+
+  @Get('search-custom-code/:code')
+  @UseGuards(AuthGuard())
+  searchCustomCode(@Param('code') customCode: string) {
+    return this.urlsService.searchCustomCode(customCode)
   }
 
   @Delete('delete/:id')
