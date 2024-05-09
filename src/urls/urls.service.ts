ae4356523e12fda0668e3aea5a64dfe39f39ba16
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-09 01:14:39 -0600

    Refactor shorten methods

--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -7,20 +7,33 @@ import { createRandomString } from './helpers/create-random-string.helper'
 export class UrlsService {
   constructor(private readonly prisma: PrismaService) { }
 
-  async shortenUrl(shortenUrlDto: ShortenUrlDto, userId: number = null) {
-    const { longUrl, customCode } = shortenUrlDto
+  async publicShortenUrl(shortenUrlDto: ShortenUrlDto) {
+    const { longUrl } = shortenUrlDto
 
-    const shortCode: string = customCode ?
-      customCode :
-      createRandomString(6)
+    let shortUrl: { shortCode: string, longUrl?: string }
 
-    const shortUrl = await this.prisma.urls.create({
-      data: {
-        longUrl,
-        shortCode,
-        createdBy: userId ? { connect: { id: userId } } : undefined
-      }
-    })
+    // verify if url already exists in database
+    const existingUrl = await this.searchUrlInDatabase(longUrl)
+
+    if (!existingUrl) {
+      // create random string
+      const shortCode: string = await this.generateNonExistingRandomString()
+
+      // create url record in db
+      shortUrl = await this.prisma.urls.create({
+        data: {
+          longUrl,
+          shortCode
+        },
+        select: {
+          shortCode: true,
+          longUrl: true
+        }
+      })
+    } else {
+      // if url already exists in db, returns its short code
+      shortUrl = existingUrl
+    }
 
     return {
       shortCode: shortUrl.shortCode,
@@ -28,9 +41,57 @@ export class UrlsService {
     }
   }
 
+  async shortenUrl(shortenUrlDto: ShortenUrlDto, userId: number) {
+    // TODO
+    await Promise.resolve()
+
+    return {
+      shortenUrlDto,
+      userId
+    }
+  }
+
+  async customShortenUrl(shortenUrlDto: ShortenUrlDto, userId: number) {
+    // TODO
+    await Promise.resolve()
+
+    return {
+      shortenUrlDto,
+      userId
+    }
+  }
+
   async deleteUrl(id: number) {
     await this.prisma.urls.delete({
       where: { id }
     })
+  }
+
+  async searchCustomCode(customCode: string) {
+    // TODO
+    await Promise.resolve()
+
+    return customCode
+  }
+
+  private async searchUrlInDatabase(longUrl: string) {
+    return await this.prisma.urls.findFirst({
+      where: { longUrl },
+      select: {
+        shortCode: true,
+        longUrl: true
+      }
+    })
+  }
+
+  private async generateNonExistingRandomString(): Promise<string> {
+    const randomString = createRandomString(6)
+
+    const randomStringExistsInDb = await this.prisma.urls.findFirst({
+      where: { shortCode: randomString }
+    })
+
+    if (!randomStringExistsInDb) return randomString
+    else return await this.generateNonExistingRandomString()
   }
 }
