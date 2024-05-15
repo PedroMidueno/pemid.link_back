95dd5ed1e32c3491f0cd216bd14ea498ae6e3f33
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-15 00:11:09 -0600

    Refactor get-user decorator

--- src/common/decorators/get-user.decorator.ts
+++ src/common/decorators/get-user.decorator.ts
@@ -1,11 +1,12 @@
-import { ExecutionContext, createParamDecorator } from '@nestjs/common'
+import { ExecutionContext, NotFoundException, createParamDecorator } from '@nestjs/common'
 import { User } from '../types'
 
 export const GetUser = createParamDecorator((data: string | null | undefined, ctx: ExecutionContext) => {
   const req = ctx.switchToHttp().getRequest()
   const user = req.user as User
 
-  if (!user) return null
+  if (!user)
+    throw new NotFoundException('user not found')
 
   return data ? user[data] : user
 })
