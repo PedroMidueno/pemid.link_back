eacd212c17ae9ee507ba77a8ca5d7f9c714b4a47
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-15 00:11:39 -0600

    Add endpoint to get user information

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,11 +1,21 @@
-import { Body, Param, Controller, ParseIntPipe, Patch, Delete, Post } from '@nestjs/common'
+import { Body, Param, Controller, ParseIntPipe, Patch, Delete, Post, Get, UseGuards } from '@nestjs/common'
 
 import { AdminService } from './admin.service'
 import { CreateUserDto, UpdateUserDto } from './dto'
+import { AuthGuard } from '@nestjs/passport'
+import { GetUser } from 'src/common/decorators/get-user.decorator'
+import { User } from 'src/common/types'
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+
+  @Get('get-user-info')
+  @UseGuards(AuthGuard())
+  getUserInformation(@GetUser() user: User) {
+    return user
+  }
 
   @Post('create-user')
   createUser(@Body() createuserDto: CreateUserDto) {
