8cef26f63507e732e3eea17dddc6c5d5ebc6e6a7
PedroMidueno <pedromidueno.dev@gmail.com>
2024-05-15 00:11:24 -0600

    Add Auth module to imports

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AdminService } from './admin.service'
 import { AdminController } from './admin.controller'
 import { CommonModule } from 'src/common/common.module'
+import { AuthModule } from 'src/auth/auth.module'
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, AuthModule],
   controllers: [AdminController],
   providers: [AdminService]
 })
