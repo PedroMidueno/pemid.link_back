f27c47c6574029cb6f2477da2a7b4ddab21266a0
PedroMidueno <pedromidueno.dev@gmail.com>
2024-08-09 17:47:09 -0600

    Change return from googleCallback controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common'
+import { Controller, Post, Body, Get, UseGuards, Req, Res } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from './auth.service'
 import { LoginDto } from './dto/login.dto'
 import { GetUser } from 'src/common/decorators/get-user.decorator'
 import { User } from 'src/common/types'
+import { ConfigService } from '@nestjs/config'
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
+    private readonly configService: ConfigService
   ) { }
 
   @Post('login')
@@ -23,10 +25,19 @@ export class AuthController {
   @Get('google-callback')
   @UseGuards(AuthGuard('google'))
   /* eslint-disable-next-line */
-  async googleCallback(@Req() req: any) {
+  async googleCallback(@Req() req: any, @Res() res: any) {
     const user = await this.authService.loginWithGoogle(req.user.email, req.user.firstName, req.user.lastName)
-
-    return user
+    const frontendUrl = this.configService.get<string>('FRONTEND_URL')
+    const userString = JSON.stringify(user)
+    res.send(/*html*/ `
+      <!DOCTYPE html>
+        <html>
+          <script>
+            window.opener.postMessage(${userString}, '${frontendUrl}')
+            window.close()
+          </script>
+      </html>
+    `)
   }
 
   // Route for test auth endpoints
