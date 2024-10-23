be1b518c7b19cb62aeae5b7c3d5e060ebc03bed7
PedroMidueno <pedromidueno.dev@gmail.com>
2024-10-22 18:52:18 -0600

    Change dto validations

--- src/admin/dto/update-password.dto.ts
+++ src/admin/dto/update-password.dto.ts
@@ -1,13 +1,16 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator'
+import { IsNotEmpty, IsOptional, IsString, Matches, MinLength } from 'class-validator'
 
 export class UpdatePasswordDto {
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
-  @MinLength(8)
-  readonly oldPassword: string
+  readonly oldPassword?: string
 
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&_-])[A-Za-z\d@$!%*#?&_-]{8,}$/, {
+    message: 'The password must have a lowercase, an uppercase, a number and a special character'
+  })
   readonly newPassword: string
 }
