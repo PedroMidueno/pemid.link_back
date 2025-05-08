0915712c392b51a4e758faf9bf78ace5aaef47ac
PedroMidueno <pedromidueno.dev@gmail.com>
2025-05-08 16:54:15 -0600

    add caching to the get long url service

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '../common/common.module'
 import { AuthModule } from '../auth/auth.module'
 import { UrlsService } from './urls.service'
 import { UrlsController } from './urls.controller'
+import { CacheModule } from '@nestjs/cache-manager'
+
+const ONE_DAY_IN_MILISECONDS = 24 * 60 * 60 * 1000
 
 @Module({
   controllers: [UrlsController],
   providers: [UrlsService],
-  imports: [AuthModule, CommonModule]
+  imports: [AuthModule, CommonModule, CacheModule.register({ ttl: ONE_DAY_IN_MILISECONDS })]
 })
 export class UrlsModule {}
